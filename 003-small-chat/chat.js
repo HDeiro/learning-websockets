f6191b2c81c88de7a932d7c214caf4608ffb42a3
Hugo Deiró <hugo.souza@wexinc.com>
2021-05-03 15:13:12 -0300

    Chat part 1

---
+++ 003-small-chat/chat.js
@@ -0,0 +1,14 @@
+const express  = require('express');
+const app      = express();
+const socketio = require('socket.io');
+
+app.use(express.static(`${__dirname}/public`));
+
+const expressServer = app.listen(8000);
+const io = socketio(expressServer);
+
+io.on('connection', socket => {
+    socket.emit('messageFromServer', {data: 'Welcome to the Socket.io Server'});
+
+    socket.on('messageToServer', dataFromClient => console.log(dataFromClient));
+})
