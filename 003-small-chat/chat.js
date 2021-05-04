d1a5721f2259be17e415e7bf46a9dd2285ded8fb
Hugo Deiró <hugo.souza@wexinc.com>
2021-05-04 16:01:05 -0300

    Quick Chat

--- 003-small-chat/chat.js
+++ 003-small-chat/chat.js
@@ -8,7 +8,13 @@ const expressServer = app.listen(8000);
 const io = socketio(expressServer);
 
 io.on('connection', socket => {
+    // Emit to the single connected client
     socket.emit('messageFromServer', {data: 'Welcome to the Socket.io Server'});
 
     socket.on('messageToServer', dataFromClient => console.log(dataFromClient));
+
+    socket.on('newMessageToServer', dataFromClient => {
+        // Emit to all connected clients
+        io.emit('messageToClients', dataFromClient);
+    });
 })
